c5a073baee0fc933658a00cab7b3fc4214c8ad2e
zengxingqi <me@zengxingqi.com>
2021-06-05 23:21:25 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: 曾星旗 <me@zengxingqi.com>
+ * @Date: 2021-06-05 23:05:08
+ * @LastEditors: 曾星旗 <me@zengxingqi.com>
+ * @LastEditTime: 2021-06-05 23:21:10
+ * @Description: socket 主入口
+ * @FilePath: /likesignal/index.js
+ */
